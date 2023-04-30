8c29eb361ce058cbd9c4b9656356d8feea8a0513
Zayyni <127606083+Zayyni@users.noreply.github.com>
2023-04-30 13:22:28 +0500

    Add files via upload

---
+++ Single_line_ifElse.py
@@ -0,0 +1,2 @@
+print("Positive") if int(input("Enter a number: "))>0 else print("Non-Positive")
+ 
---
+++ check_No_is_Pos.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the number"))
+if x>0:
+    print("Number is Positive")
+else:
+    print("Number is Non-Positive")    
+    
